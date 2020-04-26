b01035b0c5b0713353947bfcb3e98dbe677ca7ab
Eliska Klimtova <eli.klimtova@gmail.com>
2020-04-26 13:41:17 +0200

    Create components, functionality and styles

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TacoBox title="Welcome to Taco Land! 🌮"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TacoBox from './components/TacoBox.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TacoBox
   }
 }
 </script>
 
 <style>
+body {
+  background-color: #051A35;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #A3D7CD;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/TacoBox.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+    <h2>Do you want tacos?</h2>
+    <div class="tacos-wrap" v-if="wantTacos === true">
+      <h3>Here's some tacos for you!</h3>
+      <div class="tacos">{{ tacos }}</div>
+    </div>
+    <div class="no-tacos-wrap" v-if="wantTacos === false">
+      <h3>Ok, no tacos for you! ¯\_(ツ)_/¯</h3>
+    </div>
+    <button 
+      class="nope"
+      @click="noTacos"
+    >
+      Nope
+    </button>
+    <button 
+      class="yas"
+      @click="getTacos"
+    >
+      Yaaass
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TacoBox',
+  props: ['title'],
+  data() {
+    return {
+      wantTacos: '',
+      tacos: '🌮'
+    }
+  },
+  methods: {
+    getTacos() {
+      this.wantTacos = true
+    },
+    noTacos() {
+      this.wantTacos = false
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.container {
+  margin: 120px auto 0 auto;
+}
+
+h1 {
+  font-size: 64px;
+}
+
+h2 {
+  font-size: 40px;
+  margin-bottom: 70px;
+}
+
+h3 {
+  font-weight: 400;
+  font-size: 22px;
+}
+
+button {
+  border: none;
+  border-radius: 5px;
+  color: #fff;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: 600;
+  margin: 60px 20px;
+  padding: 20px;
+  transition: ease all .3s;
+  width: 150px;
+}
+
+.yas {
+  background-color: #21C481;
+}
+
+.yas:hover {
+  background-color: rgb(28, 160, 105);
+}
+
+.nope {
+  background-color: #FF5A5A;
+}
+
+.nope:hover {
+  background-color: rgb(194, 75, 75);
+}
+
+.tacos {
+  font-size: 56px;
+  margin-top: 30px;
+}
+</style>
